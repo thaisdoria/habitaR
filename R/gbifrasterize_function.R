75e45e1631fe07854eb08bb43c465d9aea14cf53
bojaum <36231883+bojaum@users.noreply.github.com>
2020-03-03 10:56:35 -0300

    Update gbifrasterize_function.R
    included new arguments.

--- R/gbifrasterize_function.R
+++ R/gbifrasterize_function.R
@@ -1,11 +1,17 @@
-f.ras <- function (list.clean){
-  for (i in 1:length(list.clean)){
-    r <- rasterize(as.data.frame(list.clean[[i]][,2:3]), r0.05, background=0, fun='count') # each coordinate point present in each cell is counted
+f.ras <- function (x=list.clean, r=r0.05, write=TRUE){
+  for (i in 1:length(x)){
+    r <- rasterize(as.data.frame(x[[i]][,2:3]), r, background=0, fun='count') # each coordinate point present in each cell is counted
     r[r > 1] <- 1 # to remove duplicates considering as one all coordinates that is present in the same cell
     c <- crop(r, caatinga)
     m <- mask(c, caatinga)
     e <- extent(-44.8, -34.8, -16.6, -2.2)
     r.occu.caat <- extend(m,e)
-    nome <- names(list.clean[i])
-    writeRaster(r.occu.caat,paste(nome,"GBIFPoints.asc",sep = ""),format="ascii", overwrite=T)
+    nome <- names(x[i])
+    if (write==TRUE)
+      {
+        writeRaster(r.occu.caat,paste(nome,"GBIFPoints.asc",sep = ""),format="ascii", overwrite=T)
+      } else {
+      return(r.occu.caat)
+      }
+    
   }}
