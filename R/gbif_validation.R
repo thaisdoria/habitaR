063e9ebe4f5963ef954ab20890ca07771790b827
Thaís Dória <thaisfdoria@gmail.com>
2020-03-10 15:51:15 -0300

    quase finalizando

--- R/gbif_validation.R
+++ R/gbif_validation.R
@@ -13,7 +13,7 @@
 #' @export validation.aoh
 
 validation.aoh <- function (eoo, aoh, resolution){
-  
+ 
   if(is.character(eoo)){
     files.sp <- list.files(eoo, pattern = ".shp$")
     files.sp <- gsub(".shp","", files.sp)
@@ -25,11 +25,11 @@ validation.aoh <- function (eoo, aoh, resolution){
     eoo <- do.call(bind, sps)
   }
   
-  
-  
-  if(class(eoo) == "SpatialPolygonsDataFrame" & class (aoh) == "SpatialPolygonsDataFrame"){
+  if(class(eoo) == "SpatialPolygonsDataFrame" & class(aoh) == "SpatialPolygonsDataFrame"){
     # rasterize the shapefiles
-    list<- list()
+    sp <- data.frame(matrix(ncol = 6, nrow = length(eoo)))
+    colnames(sp) <- c("Species", "MATCH.EOO", "MATCH.AOH", "PP", "MP", "PP-MP")
+    sp[,1] <- eoo@data[,2]
     for (i in 1:length(eoo)){
       sp.e <- eoo[i, ]
       r <- raster()
@@ -37,29 +37,83 @@ validation.aoh <- function (eoo, aoh, resolution){
       res(r) <- resolution
       sp.re <- rasterize(sp.e, r)
       sp.re [sp.re > 1] <- 1
+      plot(sp.re)
       # download occurrences from gbif based on extent of EOO (to restrict the search to inside of original distribution)
       ex <- extent(sp.re)
       names(sp.re) <- eoo@data[1,2]
       occ <- gbif(names(sp.re), ext=ex, geo=T)
       pts <- as.data.frame(cbind(occ$lon, occ$lat)) # coordinates corresponding to specified extent
       coordinates(pts) <- ~ V1 + V2
+      plot(pts, add=T)
       match.eoo <- extract (sp.re, pts)
-      match.eoo <- as.data.frame(table(match.eoo))
-      sp.a <- aoh[i, ]
-      sp.ra <- rasterize(sp.a, r)
-      match.aoh <- extract (sp.ra, pts)
-      match.aoh <- as.data.frame(table(match.aoh))
-      pp <- as.numeric(match.aoh$Freq) / as.numeric(match.eoo$Freq)
-      mp <- cellStats(sp.ra, sum) /cellStats(sp.re, sum)
-      val <- pp - mp
-      list[[i]] <- val
-    }
-    result <- do.call(rbind, list)
-    return(result)
-  }
+      
+      if (sum(match.eoo, na.rm=T) == 0) {
+        warning (paste('No occurrence record found inside the eoo raster for', as.character(spp[i])))
+        sp[i,2] <- NA
+      }
+      
+      if (sum(match.eoo, na.rm=T) != 0){
+        match.eoo <- length(match.eoo)
+        sp.a <- a[i, ]
+        sp.ra <- rasterize(sp.a, r)
+        match.aoh <- extract (sp.ra, pts)
+        match.aoh <- length(match.aoh)
+        pp <- as.numeric(match.aoh) / as.numeric(match.eoo)
+        mp <- cellStats(sp.ra, sum) /cellStats(sp.re, sum)
+        sp[i,2] <- match.eoo 
+        sp[i,3] <- match.aoh
+        sp[i,4] <- pp 
+        sp[i,5] <- mp
+        sp[i,6] <- pp - mp
+      }}  
+    return(sp)
+  plot(as.matrix(sp[,5]), as.matrix(sp[,4]), xlab = "Model Prevalence", ylab = "Point Prevalence", xlim = c(0, 1.0), ylim = c(0, 1), pch=1, cex=sqrt(as.matrix(sp[,2])))
+  abline(a = -0.0, b =1, lwd=1.0, lty=2)
+}
+
+  if(class(eoo) == "SpatialPolygonsDataFrame" & class(aoh) == "RasterLayer"){
+    sp <- data.frame(matrix(ncol = 6, nrow = length(eoo)))
+    colnames(sp) <- c("Species", "MATCH.EOO", "MATCH.AOH", "PP", "MP", "PP-MP")
+    sp[,1] <- eoo@data[,2]
+    for (i in 1:length(eoo)){
+      sp.e <- eoo[1, ]
+      r <- raster()
+      extent(r) <- extent(sp.e)
+      res(r) <- resolution
+      sp.re <- rasterize(sp.e, r)
+      sp.re [sp.re > 1] <- 1
+      ex <- extent(sp.re)
+      names(sp.re) <- eoo@data[1,2]
+      occ <- gbif(names(sp.re), ext=ex, geo=T)
+      pts <- as.data.frame(cbind(occ$lon, occ$lat)) # coordinates corresponding to specified extent
+      coordinates(pts) <- ~ V1 + V2
+      match.eoo <- as.matrix(extract (sp.re, pts))
+      
+      if (sum(match.eoo, na.rm=T) == 0) {
+        warning (paste('No occurrence record found inside the eoo raster for', as.character(spp[i])))
+        sp[i,2] <- NA
+      }
+      
+      if (sum(match.eoo, na.rm=T) != 0){
+        match.eoo <- length(match.eoo)
+        sp.a <- a[i, ]
+        sp.ra <- rasterize(sp.a, r)
+        match.aoh <- extract (sp.ra, pts)
+        match.aoh <- length(match.aoh)
+        pp <- as.numeric(match.aoh) / as.numeric(match.eoo)
+        mp <- cellStats(sp.ra, sum) /cellStats(sp.re, sum)
+        sp[i,2] <- match.eoo 
+        sp[i,3] <- match.aoh
+        sp[i,4] <- pp 
+        sp[i,5] <- mp
+        sp[i,6] <- pp - mp
+      }}  
+    return(sp)
+plot(as.matrix(sp[,5]), as.matrix(sp[,4]), xlab = "Model Prevalence", ylab = "Point Prevalence", xlim = c(0, 1.0), ylim = c(0, 1), pch=1, cex=sqrt(as.matrix(sp[,2])))
+abline(a = -0.0, b =1, lwd=1.0, lty=2)
+}
   
-  
-  if(class(eoo) == "RasterLayer" & class (aoh) == "RasterLayer"){
+  if(class(eoo) == "RasterLayer" & class(aoh) == "RasterLayer"){
     list <- list()
     for (i in 1:length(eoo)){
       sp.re <- eoo[i, ]
@@ -69,16 +123,29 @@ validation.aoh <- function (eoo, aoh, resolution){
       coordinates(pts) <- ~ V1 + V2
       match.eoo <- extract (sp.re, pts)
       match.eoo <- as.data.frame(table(match.eoo))
-      match.aoh <- extract (sp.ra, pts)
-      match.aoh <- as.data.frame(table(match.aoh))
-      pp <- as.numeric(match.aoh$Freq) / as.numeric(match.eoo@Freq)
-      mp <- cellStats(sp.ra, sum) /cellStats(sp.re, sum)
-      val <- pp - mp
-      list[[i]] <- val
-    }
-    result <- do.call(rbind, list)
-    return(result)
-  }}
+      if (sum(match.eoo, na.rm=T) == 0) {
+        warning (paste('No occurrence record found inside the eoo raster for', as.character(spp[i])))
+        sp[i,2] <- NA
+      }
+      
+      if (sum(match.eoo, na.rm=T) != 0){
+        match.eoo <- leght(match.eoo)
+        sp.a <- a[i, ]
+        sp.ra <- rasterize(sp.a, r)
+        match.aoh <- extract (sp.ra, pts)
+        match.aoh <- length(match.aoh)
+        pp <- as.numeric(match.aoh) / as.numeric(match.eoo)
+        mp <- cellStats(sp.ra, sum) /cellStats(sp.re, sum)
+        sp[i,2] <- match.eoo 
+        sp[i,3] <- match.aoh
+        sp[i,4] <- pp 
+        sp[i,5] <- mp
+        sp[i,6] <- pp - mp
+      }}  
+    return(sp)
+    plot(as.matrix(sp[,5]), as.matrix(sp[,4]), xlab = "Model Prevalence", ylab = "Point Prevalence", xlim = c(0, 1.0), ylim = c(0, 1), pch=1, cex=sqrt(as.matrix(sp[,2])))
+    abline(a = -0.0, b =1, lwd=1.0, lty=2)
+  }
+}
 
-  
-  
+
