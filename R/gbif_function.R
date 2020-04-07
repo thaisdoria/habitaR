4ec93d038ec9b63983a502e699187a6fa664cafc
thaisdoria <54991464+thaisdoria@users.noreply.github.com>
2020-04-07 16:33:40 -0300

    Delete gbif_function.R

--- R/gbif_function.R
+++
@@ -1,6 +0,0 @@
-
-f.gbif <- function (spp){
-  for (i in 1:length(spp)) {
-    p.occ=gbif(spp, ext=ex, geo=T, download=T)
-    return(as.data.frame(cbind(p.occ$species, p.occ$lon, p.occ$lat))) # returning only coordinates and name of species
-  }}
