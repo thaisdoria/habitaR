fb822f761a981918edfb4e926376a37995927b51
Thais <thaisafdoria@gmail.com>
2020-04-16 21:49:15 -0300

    frufrus

--- R/gbif_validation.R
+++ R/gbif_validation.R
@@ -3,8 +3,14 @@
 #' Provide the summary of validation results based on comparison between an
 #' independent occurrence records of species and the extent of occurrence (EOO)
 #' and area of habitat (AOH) following Rondinini et al. (2011)
-#' @usage aohVal(eoo, aoh, resolution=NULL, plot=TRUE)
-#' @param eoo Spatial distribution data of the species representing the original
+#' @usage
+#' # method for signature 'SpatialPolygonsDataFrame' (eoo and aoh)
+#' aohVal(eoo, aoh, resolution, plot=TRUE, progress=FALSE)
+#' # method for signature 'SpatialPolygonsDataFrase' (eoo) and 'RasterStack' (aoh)
+#' aohVal(eoo, aoh, plot=TRUE, progress=FALSE)
+#' # method for signature 'RasterStack' (eoo and aoh)
+#' aohVal(eoo, aoh, spplist, plot=TRUE, progress=FALSE)
+#' #' @param eoo Spatial distribution data of the species representing the original
 #' (i.e. not refined) extent of occurrence (EOO). It might correspond to
 #' SpatialPolygons in ESRI shapefile (.shp), to RasterStack file (.asc or .tif) or to a
 #' path for a folder with EOO shapefiles (.shp). If the shapefile is used, the
@@ -12,49 +18,61 @@
 #' of the shapefile.
 #' @param aoh Spatial distribution data of the species representing the area
 #' of habitat (AOH). It might correspond to SpatialPolygons in ESRI shapefile (.shp),
-#' to RasterStack file (.asc or .tif), created or not with function 'aoh', or to
-#' path for a folder with EOO shapefiles (.shp). If the shapefile is used, the
-#' name of the species must be on the second column of the attribute table.
+#' to RasterStack file (.asc or .tif), created or not with function
+#' \code{\link[habitaR]{aoh}}, or might correspond to a path for a folder with EOO shapefiles (.shp).
+#' If the shapefile is used, the name of the species must be on the second column of the attribute table.
 #' @param resolution Numeric value to indicate the resolution prefered to generate
-#' the raster files. This argument is only used if the spatial distribution data of
-#' species ('eoo' or 'aoh') are in shapefile format. In this case, it should be used the same
-#' resolution adopted to mapping the area of habitat (AOH).
-#' @param spplist Vector of species list in the same order as the rasterstack of
+#' the raster files. Only used if both spatial data ('eoo' and 'aoh') are in shapefile format.
+#' If 'eoo' is a shapefile and the 'aoh' is a raster, the resolution of latter is considered.
+#' @param spplist Species list (character type) in the same order as the Rasterstack of
 #' distribution data.
 #' @param plot (logical) Whether the output should also return a plot representing the
-#' graphical result of validation (see Rondinini et al. 2011).
+#' graphical result of validation (see Rondinini et al. 2011). Default is TRUE.
+#' @param progress (logical) a bar showing the progress of the function. Default is FALSE.
 #' @import dismo
 #' @import sp
 #' @import ggplot2
-#' @return \code{dfRes, gg} The result is a list with two objects. The first is a data.frame
-#' species-specific with a summary of data. It gives the number of records matching with the
-#' EOO ("MATCH.EOO) and with the AOH ("MATCH.AOH) and the values of prevalence of points
-#' ("PP", Rondinini et al. 2011) through the proportion of occurrences spatially congruent
-#' with suitable cells, and of model prevalence ("MP", Rondinini et al. 2011), which
-#' represents EOOs proportion assigned as suitable). AOH predict species occurrences
-#' correctly when all available records match with its suitable cells (PP = 1). The
-#' evaluation of AOHs performance is based on difference between PP and MP. If PP > MP, AOH
-#' performs better than EOO in predict presences (Ficetola et al. 2015). The second object is
-#' a ggplot object with a graphical representation of validation results (see Rondinini et al. 2011).
+#' @importFrom utils txtProgressBar setTxtProgressBar
+#' @return \code{aohVal} If plot=TRUE, the function return a list with two objects.
+#' The first is a data.frame with a summary of results for each species which aoh was evaluated.
+#' It gives the number of records matching with the EOO ("MATCH.EOO) and with the AOH ("MATCH.AOH)
+#' and the values of prevalence of points ("PP", Rondinini et al. 2011) through the proportion of
+#' occurrences spatially congruent with suitable cells, and of model prevalence ("MP",
+#' Rondinini et al. 2011), which represents EOOs proportion assigned as suitable). AOH predict
+#' species occurrences correctly when all available records match with its suitable cells (PP = 1).
+#' The evaluation of AOHs performance is based on difference between PP and MP. If PP > MP, AOH
+#' performs better than EOO in predict presences (Ficetola et al. 2015).
+#' The second object is a ggplot with a graphical representation of validation results
+#' (see Rondinini et al. 2011).
+#' If plot=FALSE, the return only the data.frame with the summary of results.
 #' @details The function give the summary of validation steps followed to evaluate
-#' the quality of models as described and performed by Rondidini et al. (2011)
+#' the quality of models as described and performed by Rondidini et al. (2011).
+#' If plot=FALSE, the function return a data.frame
 #' and Ficetola et al. (2015)
 #' @examples
-#' val_data <- aohVal(eoo = sd_amph, aoh = ref_amph, resolution = 0.05)
+#' val_data1 <- aohVal(eoo = eoo_amphShp, aoh = aoh_amphShp, resolution = 0.05, plot=TRUE, progress = TRUE)
+#' val_data2 <- aohVal(eoo = eoo_amphShp, aoh = aoh_amphRas, plot=TRUE, progress = TRUE)
+#' val_data3 <- aohVal(eoo = eoo_amphRas, aoh = aoh_amphRas, spplist = spplist_amph, plot = TRUE, progress = TRUE)
 #' @references Rondinini, C., Di Marco, M., Chiozza, F., Santulli, G., Baisero, D., Visconti, P., Boitani, L. (2011). Global habitat suitability models of terrestrial mammals. Philosophical Transactions of the Royal Society B, 366, 2633–2641. Brooks, T. M, Fonseca, S.L. Pimm, Akçakaya, H.R., Buchanan, G.M., …, Rondinini C. (2019). Measuring Terrestrial Area of Habitat (AOH) and Its Utility for the IUCN Red List. Trends in Ecology &amp; Evolution, 34(11), 977–986. Ficetola, G. F., Rondinini, C., Bonardi, A., Baisero, D., &amp; Padoa-Schippa, E. (2015).Habitat availability for amphibians and extinction threat: a global analysis. Diversity and Distributions, 21(3), 302–311.
 #' @author Thaís Dória & Daniel Gonçalves-Souza
 #' @export aohVal
 
 aohVal <- function (eoo, aoh, resolution=NULL, spplist=NULL, plot=TRUE){
 
-    {
+  {
     if (missing(eoo))
       stop("missing eoo")
     if (missing(aoh))
       stop("missing aoh")
-    if(class(eoo) == "RasterStack" & class(aoh) == "RasterStack" & is.null(spplist))
-      stop("spplist is required if eoo and aoh are both in RasterStack format")
-      }
+    if(class(eoo) == "SpatialPolygonsDataFrame" & class(aoh) == "SpatialPolygonsDataFrame" & is.null(resolution))
+      stop("resolution is required if 'eoo' and 'aoh' are in shapefile format")
+    if(class(eoo) == "SpatialPolygonsDataFrame" & class(aoh) == "RasterStack" & !is.null(resolution))
+      stop("resolution is NOT required if 'aoh' is a rasterstack (resolution will be the same as that from 'aoh')")
+    if(class(eoo) == "RasterStack" & class(aoh) == "RasterStack" & missing(spplist))
+      stop("spplist is required if eoo and aoh are in RasterStack format")
+    if(class(eoo) == "RasterStack" & class(aoh) == "RasterStack" & !is.null(resolution))
+      stop("a resolution should be the same or coarser than 'eoo' and 'aoh'")
+  }
 
   if(is.character(eoo)){
     files.sp.e <- list.files(eoo, pattern = ".shp$")
@@ -83,6 +101,9 @@ aohVal <- function (eoo, aoh, resolution=NULL, spplist=NULL, plot=TRUE){
     dfRes <- data.frame(matrix(ncol = 6, nrow = length(eoo)))
     colnames(dfRes) <- c("Species", "MATCH.EOO", "MATCH.AOH", "PP", "MP", "PP-MP")
     dfRes[,1] <- eoo@data[,2]
+    if(progress == TRUE){
+      pb <- txtProgressBar(min = 0, max = length(eoo), style = 3)
+    }
     for (i in 1:length(eoo)){
       sp.e <- eoo[i, ]
       r <- raster()
@@ -119,7 +140,9 @@ aohVal <- function (eoo, aoh, resolution=NULL, spplist=NULL, plot=TRUE){
         warning (paste('No occurrence record found inside the eoo raster for', as.character(nm)))
         dfRes[i,2:6] <- NA
       }
-
+      if(progress == TRUE){
+        setTxtProgressBar(pb, i)
+      }
     }
 
   if (plot == FALSE){
@@ -162,11 +185,14 @@ aohVal <- function (eoo, aoh, resolution=NULL, spplist=NULL, plot=TRUE){
     dfRes <- data.frame(matrix(ncol = 6, nrow = length(eoo)))
     colnames(dfRes) <- c("Species", "MATCH.EOO", "MATCH.AOH", "PP", "MP", "PP-MP")
     dfRes[,1] <- eoo@data[,2]
+    if(progress == TRUE){
+      pb <- txtProgressBar(min = 0, max = length(eoo), style = 3)
+    }
     for (i in 1:length(eoo)){
       sp.e <- eoo[i, ]
       r <- raster()
       extent(r) <- extent(sp.e)
-      res(r) <- resolution
+      res(r) <- res(aoh[[i]])
       sp.re <- rasterize(sp.e, r)
       sp.re [sp.re > 1] <- 1
       plot(sp.re)
@@ -197,7 +223,9 @@ aohVal <- function (eoo, aoh, resolution=NULL, spplist=NULL, plot=TRUE){
         warning (paste('No occurrence record found inside the eoo raster for', as.character(nm)))
         dfRes[i,2:6] <- NA
       }
-
+      if(progress == TRUE){
+        setTxtProgressBar(pb, i)
+      }
     }
 
     if (plot == FALSE){
@@ -240,6 +268,9 @@ aohVal <- function (eoo, aoh, resolution=NULL, spplist=NULL, plot=TRUE){
     dfRes <- data.frame(matrix(ncol = 6, nrow = nlayers(eoo)))
     colnames(dfRes) <- c("Species", "MATCH.EOO", "MATCH.AOH", "PP", "MP", "PP-MP")
     dfRes[,1] <- spplist
+    if(progress == TRUE){
+      pb <- txtProgressBar(min = 0, max = length(eoo), style = 3)
+    }
     for (i in 1:nlayers(eoo)){
       sp.re <- eoo[[i]]
       plot(sp.re)
@@ -270,7 +301,9 @@ aohVal <- function (eoo, aoh, resolution=NULL, spplist=NULL, plot=TRUE){
         warning (paste('No occurrence record found inside the eoo raster for', as.character(nm)))
         dfRes[i,2:6] <- NA
       }
-
+      if(progress == TRUE){
+        setTxtProgressBar(pb, i)
+      }
     }
 
     if (plot == FALSE){
