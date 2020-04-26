78f1e60183845e5507774d866a15af4aaaf1db0c
Your <thaisafdoria@gmail.com>
2020-04-26 13:49:02 -0300

    Added new function

--- R/readShp_funciton.R
+++ R/readShp_funciton.R
@@ -3,12 +3,12 @@
 #' Read the spatial distribution shapefiles (ESRI shapefile format) of species
 #' from a specified folder and provide a SpatialPolygonDataFrame object.
 #'
-#' @usage aoh(eoo, lc.rec, matrix.hab.pref, alt.map = NULL, matrix.alt.pref = NULL,
-#' shp.out = FALSE, resolution = NULL, continuous = FALSE, threshold = 0.5,
-#' extent.out = NULL, progress = FALSE)
+#' @usage readShp (path)
 #' @param path path for a folder with spatial distribution shapefiles
 #' (ESRI shapefile format). The name of the species must be on the second
 #' column of the attribute table of the shapefile.
+#' @author Thaís Dória & Daniel Gonçalves-Souza
+#' @export readShp
 
 readShp <- function(path){
   if(substr(path, nchar(path), nchar(path)) == '/'){
@@ -30,5 +30,3 @@ readShp <- function(path){
     return(sd)
   }
 }
-
-t<-readShp("G:/Backup_DELLUFBA_ABRIL2020/Documents/TESTE")
