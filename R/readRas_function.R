3038a5066e8abee6eca6725dcf9c20904aa1b9e4
Your <thaisafdoria@gmail.com>
2020-04-28 20:29:07 -0300

    encoding

--- R/readRas_function.R
+++ R/readRas_function.R
@@ -7,6 +7,7 @@
 #' @param path Path for a folder with spatial distribution raster files
 #' (.asc or .tif format).
 #' @return A list of raster files or a raster file
+#' @encoding UTF-8
 #' @author Thaís Dória & Daniel Gonçalves-Souza
 #' @export readRas
 
