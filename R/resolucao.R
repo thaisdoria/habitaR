4d0f1f0df8ecc3f4535ef278faa947a754b686e7
Daniel <dan-gs@live.com>
2020-05-17 09:34:52 -0300

    Resolution for stacks

--- R/resolucao.R
+++ R/resolucao.R
@@ -1,8 +1,35 @@
 resolucao <- function(x, y){
-  factor <- trunc(xres(y) / xres(x))
-  if(factor > 1){
-    x <- aggregate(x, fact = factor, fun = sum)
-    x <- x / (factor^2)
+  #RasterStack or #RasterBrick
+  if(class(x) %in% c('RasterBrick', 'RasterStack')){
+    new.x <- list()
+    for(i in 1:nlayers(x)){
+      new.x[[i]] <- x[[i]]
+      if(names(x) %in% 'adq'){
+        new.x[[i]] <- resample(new.x[[i]], y)
+      }
+      if(names(x) %in% c('lc', 'alt')){
+        factor <- trunc(xres(y) / xres(new.x))
+        if(factor > 1){
+          new.x[[i]] <- aggregate(new.x[[i]], fact = factor, fun = sum)
+          new.x[[i]] <- new.x[[i]] / (factor^2)
+        }
+        new.x[[i]] <- resample(new.x[[i]], y, method = 'ngb')
+      }
+    }
+    new.x <- stack(new.x)
+    return(new.x)
   }
-  x <- resample(x, y, method = 'ngb')
+  #RasterLayer
+  if(names(x) %in% 'adq'){
+    x <- resample(x, y)
+  }
+  if(names(x) %in% c('lc', 'alt')){
+    factor <- trunc(xres(y) / xres(x))
+    if(factor > 1){
+      x <- aggregate(x, fact = factor, fun = sum)
+      x <- x / (factor^2)
+    }
+    x <- resample(x, y, method = 'ngb')
+  }
+  return(x)
 }
