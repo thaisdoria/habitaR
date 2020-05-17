a1c02d372a36175d97d6520cf8aeda65a669f04d
Daniel <dan-gs@live.com>
2020-05-17 11:31:50 -0300

    Fixed progress bar and shp.out Updated resolucao Changed the arguments

--- R/aoh_function.R
+++ R/aoh_function.R
@@ -2,34 +2,38 @@
 #'
 #' Provide the area of habitat (AOH) of a given species through refinement of its
 #' known geographic distribution
-#' @usage aoh(eoo.sp, lc.rec, matrix.hab.pref, alt.map = NULL, matrix.alt.pref = NULL,
-#' shp.out = FALSE, resolution = NULL, continuous = FALSE, threshold = 0.5,
-#' extent.out = NULL, progress = FALSE)
+#' @usage aoh(eoo.sp = NULL, lc = NULL, alt = NULL, altpref = NULL,
+#' habpref = NULL, adq = NULL, threshold = 0.5, resolution = NULL,
+#' continuous = FALSE, shp.out = FALSE, extent.out = NULL,
+#' progress = FALSE)
 #' @param eoo.sp SpatialPolygons of the spatial distribution of the species or path
 #'  for a folder with spatial distribution shapefiles (ESRI shapefile format) .
 #'  The name of the species must be on the second column of the attribute table
 #'  of the shapefile.
-#' @param lc.rec RasterLayer object of the land use map reclassified for the
+#' @param lc RasterLayer, RasterBrick or RasterStack object of the land use map reclassified for the
 #' categories of habitat.
-#' @param matrix.hab.pref Data frame 0/1 of habitat preference of the species.
+#' @param habpref Data frame 0/1 of habitat preference of the species.
 #'  First column must be the species name. The posterior columns must be named
-#'  after the categories of habitat as following the lc.rec classification.
+#'  after the categories of habitat as following the lc classification.
 #'  \code{\link[habitaR]{prefHab}} can help obtain this data.
-#' @param alt.map RasterLayer object of the elevation map. Optional.
-#' @param matrix.alt.pref Data frame with altitudinal range of species. First
+#' @param alt RasterLayer object of the elevation map. Optional.
+#' @param altpref Data frame with altitudinal range of species. First
 #' column must be the species name, second column the minimum value of altitude
 #' and the third column the maximum value of altitude
+#' @param adq List containing RasterLayers objects of the climatic adequability
+#' of the species. The itens of the list must be named after their
+#' corresponding species. Optional.
 #' @param shp.out (logical) Whether the output should be a shapefile as opposed
 #' to a raster.
 #' @param resolution Numeric value indicating the preferred resolution for the
-#' rasters. Resolution must coarser than the resolution of lc.rec and alt.map.
+#' rasters. Resolution must coarser than the resolution of lc and alt.map.
 #' @param continuous (logical) Whether the output should be binary or continuous
 #' for the rasters. Only used if resolution provided or when the resolution of
-#' lc.rec and alt.map are different.
+#' lc and alt.map are different.
 #' @param threshold Numeric value indicating the threshold of the cell coverage
 #' by the species to the species be considered present (values between 0 and 1).
 #' Only used if continuous = \code{FALSE} and a coarser resolution is provided or
-#' the resolution of lc.rec and alt.map are different or shp.out is
+#' the resolution of lc and alt.map are different or shp.out is
 #' \code{TRUE}.
 #' @param extent.out Extent object or a vector of four numbers indicating the
 #' preferred output for the rasters. Optional.
@@ -49,8 +53,8 @@
 #' (EOO). This mapping is made considering the specific preference for habitats
 #' of a given species.
 #' @examples
-#' ref_data <- aoh(eoo.sp = eoo_amphShp, lc.rec = lc , matrix.hab.pref = habpref,
-#' alt.map = al, matrix.alt.pref = alpref)
+#' ref_data <- aoh(eoo.sp = eoo_amphShp, lc = lc , habpref = habpref,
+#' alt.map = al, altpref = alpref)
 #' @encoding UTF-8
 #' @author Daniel Gonçalves-Souza & Thaís Dória
 #' @export aoh
@@ -58,22 +62,31 @@
 #' @import rgeos
 #' @importFrom utils txtProgressBar setTxtProgressBar
 
-aoh <- function(eoo.sp = NULL, adq = NULL, lc = NULL, alt = NULL, altpref = NULL,
-                habpref = NULL, threshold = 0.5, resolution = NULL,
+aoh <- function(eoo.sp = NULL, lc = NULL, alt = NULL, altpref = NULL,
+                habpref = NULL, adq = NULL, threshold = 0.5, resolution = NULL,
                 continuous = FALSE, shp.out = FALSE, extent.out = NULL,
                 progress = FALSE){
+
+  # Data frame of resuts
+  df <- data.frame(matrix(ncol = 3, nrow = length(eoo.sp)))
+  names(df) <- c('Species', 'Vegetation', 'Altitude')
+  df[, 1] <- eoo.sp@data[, 2]
+  df[, 2:3] <- 1
+
   # Checklist
   {
     if (missing(eoo.sp))
       stop("eoo.sp is missing")
     if(is.null(lc) & is.null(alt))
       stop('You have to provide at least lc or alt')
-    if(!is.null(habpref) & is.null(alt))
-      stop('alt is missing')
-    if(is.null(habpref) & !is.null(alt))
-      stop('altpref is missing')
-    if (missing(habpref))
-      stop("habpref is missing")
+    if(is.null(lc))
+      df[, 2] <- 0
+    if(!is.null(habpref) & is.null(lc))
+      stop('lc is missing')
+    if(is.null(habpref) & !is.null(lc))
+      stop('habpref is missing')
+    if(is.null(alt))
+      df[, 3] <- 0
     if(!is.null(altpref) & is.null(alt))
       stop('alt is missing')
     if(is.null(altpref) & !is.null(alt))
@@ -89,12 +102,6 @@ aoh <- function(eoo.sp = NULL, adq = NULL, lc = NULL, alt = NULL, altpref = NULL
     eoo.sp <- readShp(eoo.sp)
   }
 
-  # Data frame of resuts
-  df <- data.frame(matrix(ncol = 3, nrow = length(eoo.sp)))
-  names(df) <- c('Species', 'Vegetation', 'Altitude')
-  df[, 1] <- eoo.sp@data[, 2]
-  df[, 2:3] <- 1
-
   # List of maps
   maps.names <- c('adq', 'lc', 'alt')
   present <- c(!is.null(adq), !is.null(lc), !is.null(alt))
@@ -106,6 +113,9 @@ aoh <- function(eoo.sp = NULL, adq = NULL, lc = NULL, alt = NULL, altpref = NULL
 
   # Looping for refinament
   result <- list()
+  if(progress){
+    pb <- txtProgressBar(min = 0, max = length(eoo.sp), style = 3)
+  }
   for(i in 1:length(eoo.sp)){
     maps.eoo <- lapply(maps, function(x) crop(x, eoo.sp[i, ]))
 
@@ -165,18 +175,10 @@ aoh <- function(eoo.sp = NULL, adq = NULL, lc = NULL, alt = NULL, altpref = NULL
       ref <- suppressWarnings(do.call(overlay,
                                       c(maps.eoo.mod,
                                         fun =  function(x) Reduce('*', x))))
-    }
+      }
 
     if(length(maps.eoo) == 1){
       ref <- maps.eoo[[1]]
-    }
-
-    # Change resolution
-    if(!is.null(resolution)){
-      base <- raster()
-      extent(base) <- extent(ref)
-      res(base) <- resolution
-      ref <- resolucao(ref, base)
     }
 
     # Continuous
@@ -184,14 +186,27 @@ aoh <- function(eoo.sp = NULL, adq = NULL, lc = NULL, alt = NULL, altpref = NULL
       ref <- ref > threshold
     }
 
+    # Change resolution
+    if(!is.null(resolution)){
+      base <- raster()
+      extent(base) <- extent(ref)
+      res(base) <- resolution
+      {
+      if(!continuous)
+        ref <- resolucao(ref, base, mode = 2)
+        ref <- ref > threshold
+        if(continuous)
+        ref <- resolucao(ref, base, mode = 1)
+      }
+    }
+
     # Shapefile output
     if(shp.out){
-      if(sum(values(ref > 0), na.rm = T) == 0){
+      if(minValue(ref) == 0 & maxValue(ref) == 0){
         warning(paste('Cannot return shapefile of', eoo.sp[i, ]@data[, 2],
                       'because there is no cells left after the refinement'))
         ref <- NA
-      }
-      if(sum(values(ref > 0), na.rm = T) > 0){
+      } else {
         if(class(ref) %in% c('RasterBrick', 'RasterStack')){
           ref <- lapply(as.list(ref),
                         function(x) rasterToPolygons(x,
@@ -204,6 +219,10 @@ aoh <- function(eoo.sp = NULL, adq = NULL, lc = NULL, alt = NULL, altpref = NULL
           names(ref@data) <- eoo.sp[i, ]@data[, 2]
         }
       }
+    }
+
+    if(!shp.out){
+      names(ref) <- eoo.sp@data[i, 2]
     }
 
     result[[i]] <- ref
