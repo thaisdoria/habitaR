c06a9bf9ce5aa9ba3a3fdf414a81fb0014b1f0cc
Your <thaisafdoria@gmail.com>
2020-05-20 16:25:06 -0300

    ahull function

--- R/aHull_function.R
+++ R/aHull_function.R
@@ -1,12 +1,15 @@
 #' aHull function
 #'
-#' Provide the alpha hull polygon to represent the extent of occurrence (EOO) of
-#' species from the occurrence records of multiple species. It is build based on
-#' the original function \code{\link[rangeBuilder:getDynamicAlphaHull]
-#' {getDynamicAlphaHull}} (rangeBuilder package), that determines parameter
-#' alpha by the spatial distribution of the coordinates.
+#' From the raw occurrences records of multiple species, provides the alpha hull
+#' polygon to represent the extent of occurrence (EOO) of each species which have
+#' a minimun of 3 not duplicate records. It is build based on the original function
+#' \code{\link[rangeBuilder:getDynamicAlphaHull]{getDynamicAlphaHull}}
+#' (rangeBuilder package), that determines the parameter alpha by the spatial
+#' distribution of the coordinates.
 #'
-#' @usage aHull (occ, crs, dist)
+#' @usage aHull (occ, crs, dist = 0.25, poly = NULL, fraction = 1.0, partCount = 2,
+#' buff = 1000, alphaIncrement = 0.01, cropToPoly = FALSE)
+#'
 #' @param occ Occurrences records of the species. It might be a 'sp.occ'
 #' object corresponding to a list of 'SpatialPoints' for multiple species
 #' (see \code{\link[aoh]{readOcc}} to obtain such class of object), a 'list' of
@@ -25,19 +28,40 @@
 #' longitude/latitude. For more details, see \code{\link[sp:remove.duplicates]
 #' {remove.duplicates}}.
 #' @param poly Optional. A polygon (ESRI shapefile as 'SpatialPolygonsDataFrame'
-#' class) of the specific area to be checked.
+#' class) of the specific area to be checked for occurrences of species and to
+#' restrict alpha hull development only to the species occurring inside of the polygon.
+#' See details.
 #' @param fraction The minimum fraction of occurrences that must be included in polygon.
 #' @param partCount The maximum number of disjunct polygons that are allowed.
+#' @param buff A buffer distance in meters to increase boundaries of alpha hull.
+#' @param alphaIncrement The amount to increase alpha with each iteration.
+#' @param cropToPoly (logical) Whether the output should also include the alpha
+#' hull cropped by the provided poly. Only used if poly provided.
 #' @import rangeBuilder
 #' @return \code{aHull} returns the alpha hull polygon (ESRI shapefile as
 #' 'SpatialPolygonsDataFrame') representing the extent of occurrence (EOO) of
 #' species and the respective alpha value assigned to build the provided polygon.
+#' If a \emph{poly} is provided and \emph{cropToPoly} is \code{TRUE}, \code{aHull}
+#' also returns the cropped alpha hull.
+#' #' @details The function generate an alpha hull polygon from a set of occurrences
+#' records of multiple based on derivate ahull by sequentially increasing α parameter
+#' (starting from 0 in steps of 0.01) until find the smallest value that returned one
+#' single contiguous polygon encompassing all occurrences recordsincrease
+#' If \emph{poly} is provided, the function filter the original species dataset by
+#' keeping only those species occurring inside of the polygon. In this case, the
+#' alpha hull construction is restrict to those filtered species, but the extend of
+#' these polygons can extrapolate the area of polygon if are occurrences beyond the boundaries of 'poly' objetc. If the
+#' spatial is desired, assign TRUE to 'clipToPoly'.
+#' generated
+#' , even if there are records
+#' besides this area
+#' @seealso Dynamic Alpha hulls based on increase of alpha values are created with
 #' @encoding UTF-8
 #' @author Thaís Dória & Daniel Gonçalves-Souza
 #' @export aHull
 
-aHull <- function(occ, crs, dist = NULL, poly = NULL, fraction, partCount,
-                  initialAlpha, alphaIncrement, clipToCoast, verbose = TRUE){
+aHull2 <- function(occ, crs, dist = NULL, poly = NULL, fraction, partCount, buff,
+                  alphaIncrement =  NULL, clipToPoly = FALSE){
 
   # Warning messages
   if (missing(occ))
@@ -99,15 +123,23 @@ aHull <- function(occ, crs, dist = NULL, poly = NULL, fraction, partCount,
 
   # Function to automatize the bulding of ahull to multiple-species
     f.ahull <-function(occ.ahul, spp.names){
-    al <- getDynamicAlphaHull(occ.ahul@coords, fraction=1, partCount=2, initialAlpha = 0.0, alphaIncrement=0.01, clipToCoast='no', verbose=T)
+    al <- getDynamicAlphaHull(occ.ahul@coords, fraction= fraction, partCount= partCound,
+                              initialAlpha = 0.0, alphaIncrement=alphaIncrement,
+                              clipToCoast='no', verbose=T)
     al1 <- data.frame(matrix(unlist(al[[2]])),stringsAsFactors=FALSE)
     write.table(al1, file=paste(spp.names,"alpha.csv", sep=";"))
     al2=al[[1]]
-    shapefile(al2, filename = paste(spp.names,"AHULL", sep=""), overwrite=TRUE)
+    ahul<-shapefile(al2, filename = paste(spp.names,"AHULL", sep=""), overwrite=TRUE)
+
+    if (clipToPoly == TRUE){
+      cliped<-crop(ahul, poly)
+      return(cliped)
+    }
   return(al1)
     }
 
     # Applying the function
     sp.ahull <- mapply(f.ahull, occ.ahul, spp.names)
 }
+
 
