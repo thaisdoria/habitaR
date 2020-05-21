aba539ce3d2df300acd23ae99293f507987d581e
dangs12 <dan-gs@live.com>
2020-05-21 15:32:24 -0300

    Excluded dataset of amph Added climSuit dataset Fixed info of datasets

--- R/datasets.R
+++ R/datasets.R
@@ -1,44 +1,44 @@
 #### habpref ####
-#' Habitat preference for Amphibians
+#' Habitat preference for Birds
 #'
-#' A fictitious dataset exemplifying how the data frame for the argument \code{matrix.hab.pref}
+#' A fictitious dataset exemplifying how the data frame for the argument \code{habPref}
 #' of the function \code{aoh} should be structured.
 
 #' @docType data
 #' @keywords datasets
-#' @name habpref
-#' @usage data(habpref)
-#' @format A data frame with 129 rows and 5 variables
+#' @name habpref_bird
+#' @usage data(habpref_bird)
+#' @format A data frame with 4 rows and 4 variables
 NULL
 
-#### alpref ####
-#' Altitude preference for Amphibians
+#### alpref_bird ####
+#' Altitude preference for Birds
 #'
-#' A fictitious dataset exemplifying how the data frame for the argument \code{matrix.alt.pref}
+#' A fictitious dataset exemplifying how the data frame for the argument \code{altPref}
 #' of the function \code{aoh} should be structured.
 
 #' @docType data
 #' @keywords datasets
-#' @name alpref
-#' @usage data(alpref)
-#' @format A data frame with 99 rows and 3 variables
+#' @name alpref_bird
+#' @usage data(alpref_bird)
+#' @format A data frame with 4 rows and 4 variables
 NULL
 
-#### lc ####
+#### lc_map ####
 #' Land Cover
 #'
-#' A fictitious dataset exemplifying how the raster for the argument \code{lc.rec}
+#' A fictitious dataset exemplifying how the raster for the argument \code{lc}
 #' of the function \code{aoh} should be structured.
 
 #' @docType data
 #' @keywords datasets
-#' @name lc
-#' @usage data(lc)
-#' @format A RasterLayer with 4790 rows, 3361 columns and resolution of
-#' 0.0028 x 0.0028 degrees
+#' @name lc_map
+#' @usage data(lc_map)
+#' @format A RasterLayer with 1198 rows, 841 columns and resolution of
+#' 0.01 x 0.01 degrees
 NULL
 
-#### al ####
+#### alt_map ####
 #' Altitude Map
 #'
 #' A fictitious dataset exemplifying how the raster for the argument \code{alt.map}
@@ -46,10 +46,10 @@ NULL
 
 #' @docType data
 #' @keywords datasets
-#' @name al
-#' @usage data(al)
-#' @format A RasterLayer with 2395 rows, 1681 columns and resolution of
-#' 0.005 x 0.005 degrees
+#' @name alt_map
+#' @usage data(alt_map)
+#' @format A RasterLayer with 266 rows, 187 columns and resolution of
+#' 0.05 x 0.05 degrees
 NULL
 
 #### class_ref ####
@@ -68,29 +68,32 @@ NULL
 
 if (getRversion() >= "2.15.1") utils::globalVariables('class_ref')
 
-#### AMPHIBIANS DATASET ####
+#### BIRDS DATASET ####
 
-#### eoo_amphShp ####
-#'  Extent of Occurrence' (EOO) corresponding to the original (i.e. not refined)
-#'  spatial distribution of Amphibians in class of 'SpatialPolygonsDataFrame'.
+#### eoo_birdShp ####
+#' EOO shapefile for Birds
 #'
-#' A fictitious dataset exemplifying how the raster file for the argument \code{eoo.sp}
-#' of the function \code{aohVal} could be structured. This dataset was generated
-#' based on available data from the IUCN database. To provide an example dataset
-#' for this package, the original maps were thinned and lightly custom modified.
+#' Extent of Occurrence' (EOO) corresponding to the original (i.e. not refined)
+#' spatial distribution of Birds in class of 'SpatialPolygonsDataFrame'.
+#'  A fictitious dataset exemplifying how the raster file for the argument \code{eoo.sp}
+#' of the functions \code{aoh} and \code{aohVal} could be structured. This
+#' dataset was generated based on available data from the IUCN database.
+#' To provide an example dataset for this package, the original maps were
+#' thinned and lightly custom modified.
 
 
 #' @docType data
 #' @keywords datasets
-#' @name eoo_amphShp
-#' @usage data(eoo_amphShp)
+#' @name eoo_birdShp
+#' @usage data(eoo_birdShp)
 #' @format A SpatialPolygonDataFrame with 3 features.
 NULL
 
-#### eoo_amphRas ####
-#'  Extent of Occurrence' (EOO) corresponding to the original (i.e. not refined)
-#'  spatial distribution of Amphibians in class of 'SpatialPolygonsDataFrame'.
+#### eoo_birdRas ####
+#' EOO Raster for Birds
 #'
+#'  Extent of Occurrence' (EOO) corresponding to the original (i.e. not refined)
+#'  spatial distribution of Birds in class of 'SpatialPolygonsDataFrame'.
 #' A fictitious dataset exemplifying how the raster file for the argument \code{eoo.sp}
 #' of the function \code{aohVal} could be structured. This dataset was generated
 #' based on available data from the IUCN database. To provide an example dataset
@@ -98,37 +101,53 @@ NULL
 
 #' @docType data
 #' @keywords datasets
-#' @name eoo_amphRas
-#' @usage data(eoo_amphRas)
+#' @name eoo_birdRas
+#' @usage data(eoo_birdRas)
 #' @format A RasterStack with 3 layers.
 NULL
 
-#### aoh_amphShp ####
-#' Spatial distribution corresponding to the 'Area of Habitat' (AOH) for Amphibians as
-#' SpatialPolygons in ESRI shapefile (.shp)
+
+#### aoh_birdShp ####
+#' AOH shapefile for Birds
 #'
+#' Spatial distribution corresponding to the 'Area of Habitat' (AOH) for Birds as
+#' SpatialPolygons in ESRI shapefile (.shp)
 #' A fictitious dataset exemplifying how the spatial distribution file for
 #' the argument \code{aoh} of the function \code{aohVal} could be structured.
 
 #' @docType data
 #' @keywords datasets
-#' @name aoh_amphShp
-#' @usage data(aoh_amphShp)
+#' @name aoh_birdShp
+#' @usage data(aoh_birdShp)
 #' @format A SpatialPolygons with 3 features.
 NULL
 
-#### aoh_amphRas ####
-#' Spatial distribution corresponding to 'Area of Habitat' (AOH) for Amphibians as
-#' raster file format (.asc).
+
+#### aoh_birdRas ####
+#' AOH raster for Birds
 #'
+#' Spatial distribution corresponding to 'Area of Habitat' (AOH) for Birds as
+#' raster file format (.asc).
 #' A fictitious dataset exemplifying how the raster file for the argument \code{aoh}
 #' of the function \code{aohVal} could be structured.
 
 #' @docType data
 #' @keywords datasets
-#' @name aoh_amphRas
-#' @usage data(aoh_amphRas)
+#' @name aoh_birdRas
+#' @usage data(aoh_birdRas)
 #' @format A RasterStack with 3 layers.
 NULL
 
-#### BIRDS DATASET ####
+
+#### climSuit_bird ####
+#'  Climatic suitability map for Birds
+#'
+#' A fictitious dataset exemplifying how the list file for the argument \code{climSuit}
+#' of the function \code{aoh} should be structured.
+
+#' @docType data
+#' @keywords datasets
+#' @name climSuit_bird
+#' @usage data(climSuit_bird)
+#' @format A list with 3 Rasterlayers.
+NULL
