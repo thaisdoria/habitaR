dae5f80f53271edc9ede3cae064a4ef954a279e7
thaisdoria <thaisafdoria@gmail.com>
2020-05-29 19:14:24 -0300

    improving description

--- R/checkOcc_function.R
+++ R/checkOcc_function.R
@@ -5,39 +5,46 @@
 #' a region of interest (e.g. study area, continent, country or other regions with
 #' specific boundaries).
 #'
-#' @usage checkOcc (occ, distOcc = 0.25, poly)
+#' @usage checkOcc (occ, poly, distOcc = NULL, SpOcc = FALSE)
 #' @param occ Occurrences records of the species (coordinates in decimal degrees).
-#' It might be a 'spOcc' object corresponding to a list of 'SpatialPoints' for
-#' multiple species (see \code{\link[aoh]{readOcc}} to obtain such class of object),
-#' a 'list' of 'data.frames' with the occurrences from multiple species, or a path
-#' for a folder with the species occurrences files (.csv format). Each file should
-#' corresponding to only one species and named with the corresponding species names.
-#' This file must have be 3 columns identified as "species" (species names or other
-#' identification of taxa), "long" (longitude), "lat" (latitude). NOTE: Longitude
-#' must be at a column before the latitude column.
-#' @param poly A polygon (ESRI shapefile as 'SpatialPolygonsDataFrame' class) of
-#' the specific area to be checked.
-#' #' @param distOcc A value corresponding to the minimum distance assigned to consider
-#' two coordinates as not duplicate. Values up to this distance will be consider
-#' as duplicates and removed. Units of this value must be in km. Default is zero
-#' (i.e. only exactly coincindent coordinates will be removed). For more details,
-#' see \code{\link[sp:remove.duplicates]{remove.duplicates}}. Optional and only
-#' used if 'occ' is a path for .csv files or a list of 'data.frame'. If 'occ'
-#' correspond to 'SpatialPoints', 'distOcc' should be ignored.
-#' @param SpOcc (logical) Whether the output should also include a list with
-#' features from 'SpatialPoints' class corresponding only to the occurrences of
-#' species filtered based on the poly area. Default if \code{FALSE}.
-#' @return \code{checkOcc} returns the species occurring inside of the polygon
-#' provided and their respective total of ocurrences falling in this polygon.
-#' If SpOcc is \code{TRUE}, \code{checkdOcc} also returns the 'SpatialPoints'
-#' of species filtered based on the poly area.
+#' It might be a path for a folder with the species occurrences files (.csv format),
+#' a 'list' of 'data.frames' with the occurrences from multiple species (see data
+#' examples), or a 'spOcc' object corresponding to a list of 'SpatialPoints' from
+#' multiple species (see \code{\link[aoh]{readOcc}} to obtain such object).
+#' NOTE: If path is provided, each .csv file should correspond to only one species
+#' and the file must be named with the corresponding species names. The files must
+#' have 3 columns identified as "species" (species names or other identification of taxa),
+#' "long" (longitude), "lat" (latitude). Longitude must be in a column before the
+#' latitude column.
+#' @param poly A polygon (ESRI shapefile from a 'SpatialPolygonsDataFrame' class)
+#' of the specific area to be checked.
+#' #' @param distOcc A value corresponding to the minimum distance assigned to
+#' consider two coordinates as not duplicate. Values up to this distance will
+#' correspond to duplicates and removed. Units of this value must be in km. Default
+#' is zero (i.e. only exactly coincindent coordinates will be removed).
+#' For more details,see \code{\link[sp:remove.duplicates]{remove.duplicates}}.
+#' Optional and only used if 'occ' is a path for .csv files or a list of 'data.frame'.
+#' If 'occ' correspond to 'SpatialPoints', this argument ('distOcc') should be ignored
+#' (i.e. NULL).
+#' @param SpOcc (logical) Whether the output should also return the 'SpatialPoints'
+#' of species filtered based on the poly boundaries. Default is \code{FALSE}.
+#' @return \code{checkOcc} returns a data.frame of species and the respectives
+#' occurrences matching with the provided polygon. If SpOcc is \code{TRUE},
+#' \code{checkdOcc} returns a list with two elements. The first element is the
+#' data.frame with the species and occurrences filtered by the polygon. The second
+#' is the 'SpatialPoints' of each species that match with polygon. In this case,
+#' the 'poly' is used only to reduce the species from initial dataset, but not
+#' to restrict the 'SpatialPoints' to occurrences inside of polygon. 'SpatialPoints'
+#' returned, thus, correspond to all set of occurrences and not to a subset that matched
+#' with the provided polygon (i.e. it is not cleaned to remove the records
+#' falling outside the polygon checked).
 #' @encoding UTF-8
 #' @author Thaís Dória & Daniel Gonçalves-Souza
 #' @export checkOcc
 #' @import sp
 #' @import rlist
 
-checkOcc<-function(occ, poly, distOcc = NULL, SpOcc=FALSE){
+checkOcc<-function(occ, poly, distOcc = NULL, SpOcc = FALSE){
 
   # Checklist and warning messages
   if (missing(occ))
@@ -45,7 +52,8 @@ checkOcc<-function(occ, poly, distOcc = NULL, SpOcc=FALSE){
   if (missing(poly))
     stop("poly is missing")
 
-   # Possibilities of input data
+
+  # Possibilities of input data
   # 1. Occurrences as .csv files or list of data.frame
   {
     # Path for a folder
@@ -72,7 +80,7 @@ checkOcc<-function(occ, poly, distOcc = NULL, SpOcc=FALSE){
       colnames(occ[[i]]@coords) <- c("long", "lat")
     }
     class(occ) <- "spOcc" # a 'spOcc' object
-  }
+    }
 
   # 2. Ocurrences as 'SpatialPoints' or 'spOcc' (obtained with readOcc function)
   {
@@ -95,15 +103,24 @@ checkOcc<-function(occ, poly, distOcc = NULL, SpOcc=FALSE){
       cf.occsum<- lapply(cf.occ, sum) # object with the sum of the checked records (0 represents complete absence of species inside of biome)
       cf.occsum[cf.occsum==0] <- NA
       cf.occcheck<-list.clean(cf.occsum, fun = is.na, recursive = TRUE)
+      class(cf.occcheck) <- "spOcc"
+
+      # Data frame of results
+      df.res <- data.frame (matrix(ncol = 2, nrow = length(cf.occcheck)))
+      names(df.res) <- c('Species', 'Checked Occurrences')
+      df.res[, 1] <- names(cf.occcheck)
+      for (i in 1:length(cf.occcheck)){
+      df.res[i,2] <- cf.occcheck[[i]]
+      }
 
       if(SpOcc == FALSE){
-        return(cf.occcheck)
+        return(df.res)
       }
 
       if(SpOcc == TRUE){
         sp.occcheck<-occ[match(names(cf.occcheck), names(occ))]
         sp.occcheck<-list.clean(sp.occcheck, fun = is.null, recursive = TRUE)
-        cf.occcheck.l<-list(CheckedOcc = cf.occcheck , CheckedSpatialPoints = sp.occcheck)
+        cf.occcheck.l<-list(CheckedOcc = df.res , CheckedSpatialPoints = sp.occcheck)
         return(cf.occcheck.l)
       }
     }
