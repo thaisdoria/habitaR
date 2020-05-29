acdf0cc43ac2d55477641b811f7fb076a9217f79
thaisdoria <thaisafdoria@gmail.com>
2020-05-29 19:13:53 -0300

    editing param and function - poly

--- R/aHull_function.R
+++ R/aHull_function.R
@@ -3,20 +3,21 @@
 #' From the occurrences records of multiple species, provides the alpha hull
 #' polygon representing the extent of occurrence (EOO) of each species which have
 #' a minimun of 3 not duplicate records. It is build based on the original function
-#' \code{\link[rangeBuilder]{getDynamicAlphaHull}} (rangeBuilder package), that
+#' \code{\link[rangeBuilder]{getDynamicAlphaHull}} \pkg{rangeBuilder}, that
 #' determines the α parameter by the spatial distribution of the coordinates.
 #'
-#' @usage aHull (occ, crs, alphaIncrement = 0.01, fraction = 1.0, partCount = 2,
-#' buff = 1000, distOcc = 0.25, poly = NULL, cropToPoly = FALSE)
+#' @usage aHull (occ, crs, fraction = NULL, partCount = NULL, alphaIncrement = NULL,
+#' buff = 0.0, distOcc = NULL, poly = NULL, cropToPoly = FALSE, occSum = TRUE)
 #'
-#' @param occ Occurrences records of the species. It might be a 'spOcc'
-#' object corresponding to a list of 'SpatialPoints' for multiple species
-#' (see \code{\link[aoh]{readOcc}} to obtain such class of object), a 'list' of
-#' 'data.frames' with the occurrences from multiple species, or a path for a folder
-#' with the species occurrences files (.csv format). Each file, corresponding to
-#' only one species and named with the corresponding species names, must have be 3 columns
-#' identified as "species" (species names or other identification of taxa), "long"
-#' (longitude), "lat" (latitude). NOTE: Longitude must be at a column before the
+#' @param occ Occurrences records of the species (coordinates in decimal degrees).
+#' It might be a path for a folder with the species occurrences files (.csv format),
+#' a 'list' of 'data.frames' with the occurrences from multiple species (see data
+#' examples), or a 'spOcc' object corresponding to a list of 'SpatialPoints' from
+#' multiple species (see \code{\link[aoh]{readOcc}} to obtain such object).
+#' NOTE: If path is provided, each .csv file should correspond to only one species
+#' and the file must be named with the corresponding species names. The files must
+#' have 3 columns identified as "species" (species names or other identification of taxa),
+#' "long" (longitude), "lat" (latitude). Longitude must be in a column before the
 #' latitude column.
 #' @param crs The Coordinate Reference System (CRS) specifing the projection and
 #' datum of dataset. Could be a CRS object or a character string.
@@ -34,6 +35,7 @@
 #' @param fraction The minimum fraction of occurrences that must be included in polygon.
 #' @param partCount The maximum number of disjunct polygons that are allowed.
 #' @param buff A buffer distance in meters to increase boundaries of alpha hull.
+#' Default is zero (0).
 #' @param alphaIncrement The amount to increase alpha with each iteration.
 #' @param cropToPoly (logical) Whether the output should also include the alpha
 #' hull cropped by the provided poly. Only used if poly provided.
@@ -55,7 +57,8 @@
 #' by the area of \emph{poly}, assign \code{TRUE} to 'cropToPoly'.
 #' @seealso Dynamic alpha hulls from increasing alpha values are created with
 #' \code{\link[rangeBuilder:getDynamicAlphaHull]{getDynamicAlphaHull}}
-#' @return \code{aHull} returns a 'aHull' object
+#' @return \code{aHull} returns a 'aHull' object corresponding to a list with
+#' two elements. The first element is a data.frame
 #'
 #' If the conditions cannot be satisfied, then a minimum convex hull is returned
 #' .
@@ -100,7 +103,7 @@
 #' @export aHull
 
 aHull <- function(occ, crs, fraction = NULL, partCount = NULL, alphaIncrement = NULL,
-                   buff = NULL, distOcc = NULL, poly = NULL, cropToPoly = FALSE,
+                   buff = 0, distOcc = NULL, poly = NULL, cropToPoly = FALSE,
                    occSum= FALSE){
 
   # Checking list and warning messages
@@ -111,27 +114,27 @@ aHull <- function(occ, crs, fraction = NULL, partCount = NULL, alphaIncrement = 
     stop("crs is missing")
   }
 
+  # Converting input data into a 'spOcc' object
+  if (class(occ) != "spOcc"){
   # Input data as 'data.frame' will be converted into a 'spOcc' object
-  {
-  # If ocurrences records are in .csv files of multiple species:
-  # To read the files and create a list of data.frames
-  if (is.character(occ)){
-      if(substr(occ, nchar(occ), nchar(occ)) == '/'){
-      occ <- substr(occ, 1, nchar(occ) - 1)
-    }
+       # 1. ocurrences records as .csv files of multiple species:
+       # To read the files and create a list of data.frames
+       if (is.character(occ)){
+          if(substr(occ, nchar(occ), nchar(occ)) == '/'){
+        occ <- substr(occ, 1, nchar(occ) - 1)
+        }
       files.sp <- list.files(occ, pattern = ".csv$")
-      occ <- do.call("list", lapply (files.sp, read.csv, header = TRUE))
+      occ <- do.call("list", lapply (files.sp, read.csv, sep=";", header = TRUE))
       names(occ) <- gsub(".csv", " ", files.sp)
     }
 
-    # If ocurrences records of multiple species are already in a list of 'data.frames
-  if (class(occ) == "list" & class(occ[[1]]) =="data.frame"){
-        occ <- occ
-     # Converting data.frame occurrences into 'SpatialPoints'
-     # Warning messages
+       # 2. Ocurrences records of multiple species as a list of 'data.frames
+       if (class(occ) == "list" & class(occ[[1]]) =="data.frame"){
+        # Converting data.frame occurrences into 'SpatialPoints'
+        # Warning messages
      {
   if (is.null(distOcc))
-    warning("distOcc is missing, so default (zero) is used")
+    warning("distOcc is not provided, so default (zero) is used")
   }
      occ <- lapply(occ, f.clean1) # a 'spOcc' object
        for (i in 1:length(occ)){
@@ -139,16 +142,19 @@ aHull <- function(occ, crs, fraction = NULL, partCount = NULL, alphaIncrement = 
        }
      class(occ) <- "spOcc"
   }
+
+ # Input data as 'SpatialPoints' will be converted into a 'spOcc' object
+    if(class(occ) == "list" & class(occ[[1]]) =="SpatialPoints"){
+      class(occ) <- "spOcc"
+    }
+
 }
 
-  # Checking and filtering the species occurrences based on a specified area (if 'poly is provided)
-  if (class(occ) == "spOcc" & !is.null(poly) | class(occ) == "list" &
-      class(occ[[1]]) =="SpatialPoints" & !is.null(poly)){
-  spcheck<-checkOcc(occ, poly)
-  occ<-occ[match(names(occ), names(spcheck))]
-  occ<-list.clean(occ, fun = is.null, recursive = TRUE)
+  # Checking and filtering the species occurrences based on a specified area (if 'poly' is provided)
+  if (class(occ) == "spOcc" & !is.null(poly)){
+  spcheck<-checkOcc(occ, poly, SpOcc=TRUE)
+  occ<-spcheck[[2]]
   }
-
 
     # GENERATING THE ALPHA HULL
     # Removing from dataset those species with less than 3 records
@@ -161,7 +167,7 @@ aHull <- function(occ, crs, fraction = NULL, partCount = NULL, alphaIncrement = 
     df[, 1] <- spp.names
 
     # Building the alpha hull for each species
-    sp.ahull <- mapply(f.ahull, occ.ahul, fraction, partCount, buff, alphaIncrement)
+    sp.ahull <- mapply(f.ahull, occ.ahul, MoreArgs = list(fraction, partCount, buff, alphaIncrement))
     ahulls <- sp.ahull[1,] # a 'SpatialPolygonsDataFrame' object
     alphas <- matrix(unlist(sp.ahull[2,]))
     alphas <-gsub("alpha", "", alphas[,1])
