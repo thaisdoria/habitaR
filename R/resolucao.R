43974672f8f4c13080bfd8080667a34ac4070607
Daniel <dan-gs@live.com>
2020-06-01 10:42:26 -0300

    If y == stack

--- R/resolucao.R
+++ R/resolucao.R
@@ -1,8 +1,11 @@
 resolucao <- function(x, y, type = 0, lbl = 'blank'){
+  if(class(y) %in% c('RasterBrick', 'RasterStack')){
+    y = y[[1]]
+  }
   #RasterStack or #RasterBrick
-  if(class(x) %in% c('RasterBrick', 'RasterStack')){
+  if (class(x) %in% c('RasterBrick', 'RasterStack')) {
     new.x <- list()
-    for(i in 1:nlayers(x)){
+    for (i in 1:nlayers(x)) {
       new.x[[i]] <- x[[i]]
       if(lbl %in% 'adq' | type == 1){
         new.x[[i]] <- resample(new.x[[i]], y)
