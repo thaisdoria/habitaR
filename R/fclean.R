bed9aa56273394f4c86d96ab6b0234c098662215
thaisdoria <thaisafdoria@gmail.com>
2020-10-27 14:24:32 -0300

    renaming function f.clean2 to f.clean

--- R/fclean.R
+++ R/fclean.R
@@ -3,7 +3,7 @@
 #' @param occ A 'spOcc' object with 'SpatialPoints' features.
 #' @noRd
 
-f.clean2 <- function (occ){
+f.clean <- function (occ){
   options(digits=4)
   sp.clean <- nrow(coordinates(occ@coords)) < 3
 }
